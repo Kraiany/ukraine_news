925d320c2837d3c7b0a4cce2476327182654a4c6
Yuri Kovalov <yuri@yurikoval.com>
2014-09-02 06:06:45 +0900

    Add article scraper.

--- lib/tasks/scrape.rake
+++ lib/tasks/scrape.rake
@@ -1,7 +1,17 @@
 require 'ua_pravda_list_scraper'
+require 'ua_pravda_article_scraper'
 namespace :scrape do
   desc "Scrape list"
   task list: [:environment] do
-    crawl_result = UaPravdaListScaper.new.crawl
+    UaPravdaListScaper.new.crawl
+  end
+
+  desc "Scrape article"
+  task article: [:environment] do
+    crawler = UaPravdaArticleScaper.new
+    Article.unscraped.each do |article|
+      crawler.article = article
+      crawler.crawl
+    end
   end
 end
---
+++ lib/ua_pravda_article_scraper.rb
@@ -0,0 +1,27 @@
+class UaPravdaArticleScaper
+  include Wombat::Crawler
+  attr_accessor :article
+
+  base_url "http://www.pravda.com.ua"
+
+  article do
+    content "css=body > div.block0 > div.pad0 > div.fblock > div.block61 > div .text", :html
+    published_at "css=body > div.block0 > div.pad0 > div.fblock > div.block61 > div > span.dt2"
+  end
+
+  def crawl
+    self[:path] = @article.relative_url
+    puts "Scraping #{self[:base_url]}#{self[:path]}"
+    @result = super
+    process_result
+  end
+
+  def process_result
+    scraped_article = @result["article"]
+    @article.content = scraped_article["content"].encode('utf-8')
+    @article.published_at = Time.parse(scraped_article["published_at"]) if scraped_article["published_at"].present?
+    @article.article_scraped_at = Time.zone.now
+    @article.state = 'scraped'
+    @article.save
+  end
+end

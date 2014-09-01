6124453aaa3c0063dcede4c5ff7a888103b517d9
Yuri Kovalov <yuri@yurikoval.com>
2014-09-02 06:40:37 +0900

    Show meta for index.

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,5 +1,17 @@
 class Api::ArticlesController < Api::BaseController
   actions :all, only: %i[index show]
+
+  def index(options={}, &block)
+    puts collection.methods
+    options = options.merge( { meta: {
+      total_entries: collection.total_count,
+      current_page: collection.current_page,
+      total_pages: collection.total_pages,
+      per_page: collection.limit_value
+    } } )
+
+    index!(options)
+  end
   protected
     def collection
       @articles ||= end_of_association_chain.scraped.order('published_at DESC').page(params[:page])

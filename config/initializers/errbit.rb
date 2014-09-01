99f8632b65fe134be980a4c77671aeb063635263
Yuri Kovalov <yuri@yurikoval.com>
2014-09-02 06:55:48 +0900

    Add errbit config.

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+Airbrake.configure do |config|
+  config.api_key = ENV['ERRBIT_KEY']
+  config.host    = ENV['ERRBIT_HOST']
+  config.port    = ENV['ERRBIT_PORT']
+  config.secure  = config.port == 443
+end

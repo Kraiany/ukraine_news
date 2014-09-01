6e4e5d498bf551b0892af25b67be4fe50a7dd915
Yuri Kovalov <yuri@yurikoval.com>
2014-09-02 00:04:34 +0900

    Add published_at to articles.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901121721) do
+ActiveRecord::Schema.define(version: 20140901150415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140901121721) do
     t.datetime "article_scraped_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "published_at"
   end
 
   add_index "articles", ["article_type"], name: "index_articles_on_article_type", using: :btree

9abedb4b73145c501975e91dbae0e9c39f3244c9
Yuri Kovalov <yuri@yurikoval.com>
2014-09-08 12:12:06 +0900

    Add paper_trail to article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  has_paper_trail :on => [:update, :destroy], :ignore => %i[state updated_at next_scrape_at scrape_with_no_changes_count]
   before_save { state ||= 'unscraped' }
   scope :unscraped, -> { where(state: 'unscraped') }
   scope :scraped, -> { where(state: 'scraped') }

fb1aceeaf2b7b79802ef46ebb84ba384857a5ca0
Yuri Kovalov <yuri@yurikoval.com>
2014-09-08 14:29:24 +0900

    Add scope for needs_rescraping.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ActiveRecord::Base
   before_save { state ||= 'unscraped' }
   scope :unscraped, -> { where(state: 'unscraped') }
   scope :scraped, -> { where(state: 'scraped') }
+  scope :needs_rescraping, -> { where('(next_scrape_at < ? OR next_scrape_at IS NULL) AND state = ?', Time.zone.now, 'scraped')}
   after_initialize { self.next_scrape_at ||= 10.minutes.from_now }
   after_initialize { self.scrape_with_no_changes_count ||= 0 }
   before_save :mark_content_changes

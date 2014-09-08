5962615823508d691366cea3c9942d80e82b2198
Yuri Kovalov <yuri@yurikoval.com>
2014-09-08 11:42:15 +0900

    Set default next_scrape_at.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   before_save { state ||= 'unscraped' }
   scope :unscraped, -> { where(state: 'unscraped') }
   scope :scraped, -> { where(state: 'scraped') }
+  after_initialize { self.next_scrape_at ||= 10.minutes.from_now }
 end

9e0f37db469d78936988f89f389014ced1143a64
Yuri Kovalov <yuri@yurikoval.com>
2014-09-08 12:07:41 +0900

    Add paper_trail.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908024403) do
+ActiveRecord::Schema.define(version: 20140908053506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,16 @@ ActiveRecord::Schema.define(version: 20140908024403) do
   add_index "articles", ["next_scrape_at"], name: "index_articles_on_next_scrape_at", using: :btree
   add_index "articles", ["state"], name: "index_articles_on_state", using: :btree
   add_index "articles", ["type"], name: "index_articles_on_type", using: :btree
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end

32d08cb3c96fa025cbc299c51d1eab017dedb123
Yuri Kovalov <yuri@yurikoval.com>
2014-09-08 11:45:21 +0900

    Add scrape_with_no_changes_count to articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ActiveRecord::Base
   scope :unscraped, -> { where(state: 'unscraped') }
   scope :scraped, -> { where(state: 'scraped') }
   after_initialize { self.next_scrape_at ||= 10.minutes.from_now }
+  after_initialize { self.scrape_with_no_changes_count ||= 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908023925) do
+ActiveRecord::Schema.define(version: 20140908024403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140908023925) do
     t.datetime "updated_at"
     t.datetime "published_at"
     t.datetime "next_scrape_at"
+    t.integer  "scrape_with_no_changes_count", default: 0
   end
 
   add_index "articles", ["next_scrape_at"], name: "index_articles_on_next_scrape_at", using: :btree

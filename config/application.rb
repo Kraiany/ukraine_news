41182b71ca64f54381bf0f1987e708b514662632
Yuri Kovalov <yuri@yurikoval.com>
2014-09-18 18:48:51 +0900

    Enable rack gzip compression.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module UkraineNews
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater # Compress with gzip
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

0887019afa3d490c4725a6c09a10a413b979c7e6
Yuri Kovalov <yuri@yurikoval.com>
2014-10-01 14:30:03 +0900

    Parse correct pravda time.

--- lib/uk_pravda_article_scraper.rb
+++ lib/uk_pravda_article_scraper.rb
@@ -1,4 +1,11 @@
 class UkPravdaArticleScraper < ArticleScraper
   content "css=body > div.block0 > div.pad0 > div.fblock > div.block61 > div .text", :html
   published_at "css=body > div.block0 > div.pad0 > div.fblock > div.block61 > div > span.dt2"
+
+  def published_at
+    @published_at ||= begin
+      short_date = result['published_at'].gsub(/^[[:word:]]+, /, '')
+      DateTime.parse_international("#{short_date} (EEST)")
+    end
+  end
 end

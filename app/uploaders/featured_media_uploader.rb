32aba85e80262dc094c3c4231e55410e9d33dee2
Yuri Kovalov <yuri@yurikoval.com>
2014-10-04 18:01:06 +0900

    Use the uploader.

---
+++ app/uploaders/featured_media_uploader.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+class FeaturedMediaUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MimeTypes
+  include CarrierWave::RMagick
+  include CarrierWaveDirect::Uploader
+  include Sprockets::Rails::Helper
+  process :set_content_type
+
+  version :thumb do
+    process resize_to_fill: [60, 35]
+  end
+
+  def store_dir
+    "featured_media/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end

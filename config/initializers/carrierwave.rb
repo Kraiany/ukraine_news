0defb6e96e314f76c25e2474b517d01114a2b151
Yuri Kovalov <yuri@yurikoval.com>
2014-10-05 00:52:30 +0900

    Add config for asset_host for carrierwave.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = ENV['FOG_DIRECTORY']
+  config.asset_host = ENV['ASSET_URI']
 end

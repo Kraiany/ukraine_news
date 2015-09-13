082d4d435b62bfd7a524d2ea939dfee2fa97bb3c
Yuri Kovalov <yuri@yurikoval.com>
2015-09-13 14:05:00 +0900

    Revive express

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module UkraineNews
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :uk
     config.autoload_paths << Rails.root.join('lib')
-    config.assets.precompile += %w( application.js application.css )
+    config.assets.precompile += %w( application.js application.css ukraine_news.js ukraine_news.css )
     config.action_controller.asset_host = ENV['ASSET_URI']
   end
 end

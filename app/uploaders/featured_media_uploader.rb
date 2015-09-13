6e205911e29632561c0c9d7dcfd5975e294654ee
Yuri Kovalov <yuri@yurikoval.com>
2015-09-13 13:27:34 +0900

    Add more media sizes

--- app/uploaders/featured_media_uploader.rb
+++ app/uploaders/featured_media_uploader.rb
@@ -11,6 +11,14 @@ class FeaturedMediaUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [60, 35]
   end
 
+  version :summarize_thumb do
+    process resize_to_fill: [230, 230]
+  end
+
+  version :size_80_80 do
+    process resize_to_fill: [80, 80]
+  end
+
   def store_dir
     "featured_media/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end

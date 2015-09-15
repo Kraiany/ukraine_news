658b66ec9fa8f1a8932b74a1b939f39261db4627
Yuri Kovalov <yuri@yurikoval.com>
2015-09-15 10:13:42 +0900

    Fix express articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     resources :articles, only: %w[index show], :defaults => { format: 'json' }
   end
   resources :articles, only: [:show, :index]
-  resources :express, only: [:show, :index]
+  resources :express, only: [:index]
+  get '/express/articles/index' => 'express#index'
   get '/express/articles/:id' => 'express#show'
 
   get '/sitemap' => redirect("#{ENV['ASSET_URI']}/sitemaps/sitemap.xml.gz"), as: :sitemap
